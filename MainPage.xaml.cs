538412ca1e984295fbba3da669b18f19a3e0c554
DesmondANIMUS <desmondmiles36@gmail.com>
2016-08-05 22:59:55 +0530

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,161 @@
+﻿using Marvel_Scouter.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Marvel_Scouter
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // Careful with its use. 3000 calls per day is the limit
+
+        public ObservableCollection<Character> MarvelCharcters { get; set; }
+        public ObservableCollection<Comic_s> MarvelComics { get; set; }
+        private List<string> suggestions;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            MarvelCharcters = new ObservableCollection<Character>();
+            MarvelComics    = new ObservableCollection<Comic_s>();
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            var storageFile = await Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///VoiceCommandDictionary.xml"));
+            await Windows.ApplicationModel.VoiceCommands.VoiceCommandDefinitionManager.InstallCommandDefinitionsFromStorageFileAsync(storageFile);
+            try
+            {
+                MyProgressRing.IsActive = true;
+                MyProgressRing.Visibility = Visibility.Visible;
+
+                while(MarvelCharcters.Count < 20)
+                {
+                    Task t =  MarvelFacade.PopulateMarvelCharactersAsync(MarvelCharcters);
+                    await t;
+                }
+
+                MyProgressRing.IsActive = false;
+                MyProgressRing.Visibility = Visibility.Collapsed;                
+            }
+
+            catch(Exception) { }
+        }
+
+        private void autoBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {           
+            suggestions = MarvelCharcters.Where(p => p.name.StartsWith(sender.Text)).Select(p => p.name).ToList();
+            autoBox.ItemsSource = suggestions;
+        }
+
+        private async void MasterListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            try
+            {
+                MyProgressRing.IsActive = true;
+                MyProgressRing.Visibility = Visibility.Visible;
+          
+                var selectedCharacter = (Character)e.ClickedItem;
+
+                DetailNameTextBlock.Text = selectedCharacter.name;
+                DetailDescriptionTextBlock.Text = selectedCharacter.description;
+
+                var larImage = new BitmapImage();
+                Uri uri = new Uri(selectedCharacter.thumbnail.large, UriKind.Absolute);
+                larImage.UriSource = uri;
+                DetailImage.Source = larImage;
+
+                MarvelComics.Clear(); // Clearing up last data (if any)
+
+                await MarvelFacade.PopulateMarvelComicsAsync(selectedCharacter.id, MarvelComics);
+
+                ComicNameTextBlock.Text = null;
+                ComicDescriptionTextBlock.Text = null;
+                ComicImage = null;
+
+
+                MyProgressRing.IsActive = false;
+                MyProgressRing.Visibility = Visibility.Collapsed;
+            }
+
+            catch (Exception) { }
+        }
+
+        private void ComicsGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            try
+            {
+                MyProgressRing.IsActive = true;
+                MyProgressRing.Visibility = Visibility.Visible;
+
+                var selectedComic = (Comic_s)e.ClickedItem;
+
+                ComicNameTextBlock.Text = selectedComic.title;
+                ComicDescriptionTextBlock.Text = selectedComic.description;
+
+                var larImage = new BitmapImage();
+                Uri uri = new Uri(selectedComic.thumbnail.large, UriKind.Absolute);
+                larImage.UriSource = uri;
+
+                ComicImage.Source = larImage;
+
+                MarvelComics.Clear(); // Clearing up last data (if any)
+
+               // await MarvelFacade.PopulateMarvelComicsAsync(selectedCharacter.id, MarvelComics);
+
+                MyProgressRing.IsActive = false;
+                MyProgressRing.Visibility = Visibility.Collapsed;
+            }
+
+            catch (Exception) { }
+        }
+
+        public async void refresh()
+        {
+            try
+            {
+                MyProgressRing.IsActive = true;
+                MyProgressRing.Visibility = Visibility.Visible;
+
+                MarvelCharcters.Clear();
+                while (MarvelCharcters.Count < 20)
+                {
+                    Task t = MarvelFacade.PopulateMarvelCharactersAsync(MarvelCharcters);
+                    await t;
+                }
+
+                MyProgressRing.IsActive = false;
+                MyProgressRing.Visibility = Visibility.Collapsed;
+            }
+
+            catch (Exception) { }
+        }
+
+        private async void autoBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            try { await MarvelFacade.getCharactersByQuery(MarvelCharcters, autoBox.Text); }
+            catch (Exception) { }         
+        }
+    }    
+}
---
+++ MarvelFacade.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.Security.Cryptography;
+using System.Runtime.Serialization.Json;
+using Windows.Security.Cryptography.Core;
+using Marvel_Scouter.Models;
+using System.Collections.ObjectModel;
+
+namespace Marvel_Scouter
+{
+    public class MarvelFacade
+    {
+        private const int randHash = 1500;
+        private const string privateKey = "6e43fc7788fa6f40499670eee8722905794ba863";
+        private const string publicKey = "a1fc304c600a9dc5f0d6d014718c5714";
+        private const string imageNotAvail = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
+
+
+        #region Comic Characters
+        public static async Task PopulateMarvelCharactersAsync(ObservableCollection<Character> marvelCharacters)
+        {
+            var characterDataWrapper = await GetCharacterDataWrapperAsync();
+            var characters = characterDataWrapper.data.results;
+
+            foreach (var chars in characters)
+            {
+                // filtering characters that are missing thumbnail images
+
+                if ((chars.thumbnail != null) && (chars.thumbnail.path != "") && (chars.thumbnail.path != imageNotAvail))
+                {
+                    chars.thumbnail.small = String.Format("{0}/standard_small.{1}", chars.thumbnail.path, chars.thumbnail.extension);
+                    chars.thumbnail.large = String.Format("{0}/standard_xlarge.{1}", chars.thumbnail.path, chars.thumbnail.extension);
+
+                    marvelCharacters.Add(chars);
+                }
+            }
+        }
+
+
+        internal static async Task getCharactersByQuery(ObservableCollection<Character> characters, string name)
+        {
+            await PopulateMarvelCharactersAsync(characters);
+            var filter = characters.Where(p => p.name == name).ToList();
+            characters.Clear();
+            filter.ForEach(p => characters.Add(p));
+        }
+
+
+        private static async Task<CharacterDataWrapper> GetCharacterDataWrapperAsync()
+        {
+            Random random = new Random();
+            var offset = random.Next(1500);
+
+            string url = String.Format("http://gateway.marvel.com:80/v1/public/characters?limit=30&offset={0}", offset);
+
+            // Call out to Marvel, then we get a response in a string 
+
+            var jsonMessage = await CallMarvelAsync(url);
+
+            // That string will represent JSON
+            // Then we de-serialize that 
+
+            var sereializer = new DataContractJsonSerializer(typeof(CharacterDataWrapper));
+            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonMessage));
+
+            var result = (CharacterDataWrapper)sereializer.ReadObject(memoryStream);
+
+            return result;
+        }
+        #endregion
+
+        #region Hash and Stuff
+        private static string CreateHash(string timeStamp)
+        {
+            var toBeHashed = timeStamp + privateKey + publicKey;
+            var hashedMessage = ComputeMD5(toBeHashed);
+
+            return hashedMessage;
+        }
+
+        private static string ComputeMD5(string s)
+        {
+            var alg = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Md5);
+            IBuffer buff = CryptographicBuffer.ConvertStringToBinary(s, BinaryStringEncoding.Utf8);
+            var hashed = alg.HashData(buff);
+            var res = CryptographicBuffer.EncodeToHexString(hashed);
+
+            return res;
+        }
+        #endregion
+
+        #region Comics
+        private static async Task<ComicDataWrapper> GetComicDataWrapperAsync(int characterId)
+        {
+            var url = String.Format("http://gateway.marvel.com:80/v1/public/comics?characters={0}&limit=10", characterId);
+            var jsonMessage = await CallMarvelAsync(url);
+
+            // That string will represent JSON
+            // Then we de-serialize that 
+
+            var sereializer = new DataContractJsonSerializer(typeof(ComicDataWrapper));
+            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonMessage));
+
+            var result = (ComicDataWrapper)sereializer.ReadObject(memoryStream);
+
+            return result;
+        }
+
+        #region Call Marvel
+        private async static Task<string> CallMarvelAsync(string url)
+        {
+            // Get MD5 Hash
+            var timeStamp = DateTime.Now.Ticks.ToString(); // timeStamp can be any string that can change frm time to time request basis
+            var hash = CreateHash(timeStamp);
+
+
+            string completeUrl = String.Format("{0}&apikey={1}&ts={2}&hash={3}", url, publicKey, timeStamp, hash);
+
+            // Call out to Marvel, then we get a response in a string 
+
+            HttpClient http = new HttpClient();
+            var response = await http.GetAsync(completeUrl); // This point we have response in form of HttpClient
+
+            return await response.Content.ReadAsStringAsync();
+        }
+        #endregion
+
+        public static async Task PopulateMarvelComicsAsync(int characterId, ObservableCollection<Comic_s> marvelComics)
+        {
+            var comicDataWrapper = await GetComicDataWrapperAsync(characterId);
+            var comics = comicDataWrapper.data.results;
+
+            foreach (var comic in comics)
+            {
+                // filtering characters that are missing thumbnail images
+
+                if ((comic.thumbnail != null) && (comic.thumbnail.path != "") && (comic.thumbnail.path != imageNotAvail))
+                {
+                    comic.thumbnail.small = String.Format("{0}/portrait_medium.{1}", comic.thumbnail.path, comic.thumbnail.extension);
+                    comic.thumbnail.large = String.Format("{0}/portrait_xlarge.{1}", comic.thumbnail.path, comic.thumbnail.extension);
+
+                    marvelComics.Add(comic);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
